536116ab9e9b7d955035bde601e948dda3b5fb1e
ParmbeerJohal <parmbeerjohal@gmail.com>
2020-06-07 12:04:30 -0700

    Uploading assignment 1

--- Assignment1/Aggregate.java
+++ Assignment1/Aggregate.java
@@ -1,214 +1,304 @@
-/* Aggregate_Retry.java
-   CSC 225 - Summer 2018
-   Parm Johal
-   V00787710
-   06/23/2018
-*/
+/*
+ * Name: Parm Johal
+ * ID: V00787710
+ * Date: June 23, 2018
+ * Filename: Aggregate.java
+ * Details: \CSC 225\ Assignment 1
+ */
 
+/*For all asymptotic time complexity analysis in this code, take n to be the number of rows (since
+  the width of a row is already constrained to 100000 characters).*/
 
-import java.io.*;
-import java.util.*;
+//For reading files:
+import java.io.BufferedReader;
+import java.io.FileReader;
+//For storing an unknown number of columns:
+import java.util.ArrayList;
+//For error handling:
+import java.io.IOException;
 
-public class Aggregate_Retry{
-	//Starter code given by Bill Bird - 04/30/2018
-	public static void showUsage(){
-		System.err.printf("Usage: java Aggregate <function> <aggregation column> <csv file> <group column 1> <group column 2> ...\n");
-		System.err.printf("Where <function> is one of \"count\", \"count_distinct\", \"sum\", \"avg\"\n");
-	}     
-	
-	public static void main(String[] args){
+public class Aggregate1{
 
-		//At least four arguments are needed
-		if (args.length < 4){
-			showUsage();
-			return;
-		}
-		String agg_function = args[0];
-		String agg_column = args[1];
-		String csv_filename = args[2];
-		String[] group_columns = new String[args.length - 3];
-		for(int i = 3; i < args.length; i++)
-			group_columns[i-3] = args[i];
-
-		if (!agg_function.equals("count") && !agg_function.equals("count_distinct") && !agg_function.equals("sum") && !agg_function.equals("avg")){
-			showUsage();
-			return;
-		}
-
-		BufferedReader br = null;
-
+	public static ArrayList<ArrayList<String>> readCSV(String inputFile) throws IOException{//Runs in O(n^2) time.
+		//Used code from https://stackoverflow.com/questions/4716503/reading-a-plain-text-file-in-java as a template for my use of BufferedReader and FileReader.
 		try{
-			br = new BufferedReader(new FileReader(csv_filename));
-		}catch( IOException e ){
-			System.err.printf("Error: Unable to open file %s\n",csv_filename);
-			return;
-		}
-
-		String header_line;
-		try{
-			header_line = br.readLine(); //The readLine method returns either the next line of the file or null (if the end of the file has been reached)
-		} catch (IOException e){
-			System.err.printf("Error reading file\n", csv_filename);
-			return;
-		}
-		if (header_line == null){
-			System.err.printf("Error: CSV file %s has no header row\n", csv_filename);
-			return;
-		}
-
-		//Split the header_line string into an array of string values using a comma
-		//as the separator.
-		String[] column_names = header_line.split(",");
-
-		//... Your code here ...
-		
-		//System.out.println(Arrays.toString(column_names));
-		
-		ArrayList<String[]> table = new ArrayList<String[]>();
-		table.add(column_names);
-		try{
-			while(br.ready()) {
-				String[] lineToRead = br.readLine().split(",");
-				table.add(lineToRead);
-			}
-		} catch(IOException e){
-			System.err.printf("Error reading file\n", csv_filename);
-			return;
-		}
-		for(String[] row : table) {
-			System.out.println(Arrays.toString(row));
-		}
-		
-		//FINISHED READING IN TABLE OF GRADES***
-		
-		//For this tablecut array
-		//Row size is equal to number of rows + header row
-		//Column size is equal to the length of group column array
-		//plus the aggregation column
-		String[][] tableCut = new String[table.size()][group_columns.length + 1];
-		
-		//ADD IN GROUP_COLUMN HEADERS TO NEW TABLE
-		for(int count = 0; count < group_columns.length; count++) {
-			tableCut[0][count] = group_columns[count];
-		}
-		//Add in agg column
-		tableCut[0][group_columns.length] = agg_column;
-		
-		//add in relevant data for each column header
-		int i;
-		//This loop searches for which column place the agg column is in initially
-		for(i = 0; i < column_names.length; i++) {
-			if(agg_column.equals(column_names[i]))
-				break;
-		}
-		for(int num = 1; num < table.size(); num++) {
-			tableCut[num][group_columns.length] = table.get(num)[i];
-		}
-		//System.out.println(Arrays.deepToString(tableCut));
-		
-		int groupCount = 0;
-		while(groupCount != group_columns.length) {
-			
-			int index = 0;
-			for(index = 0; index < table.get(0).length; index++) {
-				if(group_columns[groupCount].equals(table.get(0)[index])){break;}
-				
-			}
-			//System.out.println("index variable: " + index);
-			for(int x = 1; x < table.size(); x++) {
-				tableCut[x][groupCount] = table.get(x)[index];
-			}
-			
-			
-			groupCount++;
-		}
-		//System.out.println(Arrays.deepToString(tableCut));
-		for(String[] row : tableCut) {
-			System.out.println(Arrays.toString(row));
-		}
-		
-		ArrayList<String[]> finalTable;
-		if(agg_function.equals("count")) {
-			finalTable = count(tableCut);
-		} else if(agg_function.equals("sum")) {
-			finalTable = sum(tableCut);
-		} else if(agg_function.equals("avg")) {
-			finalTable = avg(tableCut);
-		} else {
-			finalTable = count_distinct(tableCut);
-		}
-		System.out.println("Final table to output:");
-		for(String[] row : finalTable) {
-			System.out.println(Arrays.toString(row));
-		}
-	}
-	//WORKING CODE UP UNTIL THIS POINT***
-	
-	//FINISHED COUNT METHOD***
-	public static ArrayList<String[]> count(String[][] table) {
-		int count = 0;
-		ArrayList<String[]> aggTable = new ArrayList<String[]>();
-		aggTable.add(table[0]);
-		
-		System.out.println("aggTable input:");
-		for(String[] row : aggTable) {
-			System.out.println(Arrays.toString(row));
-		}
-		
-		//Iteration to go through pruned table
-		for(int i = 1; i < table.length-1; i++){
-			if(table[i][0] == "") continue; // If 
-			String[] row = new String[table[0].length];
-			row[0] = table[i][0];
-			count = 1;
-			for(int j = i+1; j < table.length; j++) {
-				//if(table[j][0].equals(table[i][0])) { // Add new condition in if statement which checks if all group columns are the same (METHOD)
-				if(compareRows(table, i, j)) {	
-					count++;
-					table[j][0] = "";
+			BufferedReader input = new BufferedReader(new FileReader(inputFile));
+			String newLine;
+			String[] newLineList;
+			ArrayList<ArrayList<String>> table = new ArrayList<ArrayList<String>>();
+			int lineCount = 0;
+			int lastComma;
+			int columnWidth = 0;//Initialized to prevent "might not have been initialized" error java keeps giving me...
+			while ((newLine = input.readLine()) != null){//
+				if (newLine.trim().equals("")){
 					continue;
 				}
+				table.add(new ArrayList<String>());/*Takes n operations, since new space must be allocated when the
+													 ArrayList has filled it's previously allocated space.*/
+				lastComma = 0;
+				newLineList = newLine.split(",");//Irrelevant for time complexity since newLine has a maximum length of 100000 characters.
+				for (int i = 0; i < newLineList.length; i++){//Runs a max of 100001 times.
+					table.get(lineCount).add(newLineList[i]);
+				}
+				if (lineCount == 0){
+					columnWidth = table.get(lineCount).size();
+				}
+				else{
+					if (table.get(lineCount).size() != columnWidth){
+						throw new IOException("Table contains inconsistent number of columns.");
+					}
+				}
+				lineCount++;
 			}
-			//System.out.println("after nested for loop\nrow index accessed is:" + table[0].length);
-			row[table[0].length - 1] = Integer.toString(count);
-			aggTable.add(row);
+			input.close();
+			return table;
+		} catch (IOException e) {
+			throw new IOException("Can't find file" + inputFile);
 		}
-		
-		System.out.println("aggTable to return:");
-		for(String[] row : aggTable) {
-			System.out.println(Arrays.toString(row));
+	}
+
+	public static String[][] reformat(ArrayList<ArrayList<String>> inputData, String[] groups, String aggregation) throws IOException{//Runs in O(n) time.
+		for (int i = 0; i < groups.length; i++){
+			if (groups[i].equals(aggregation)){
+				throw new IOException("Group column matches aggregation column.");
+			}
 		}
-		
-		return aggTable;
+		int width = groups.length + 1;
+		int height = inputData.size();
+		String[][] outputData = new String[height][width];
+		int[] columnIndices = new int[width];
+		int cc = 0;
+		boolean aggregationFound = false;
+		for (int i = 0; i < inputData.get(0).size(); i++){//For all rows (except 0th row):
+			for (int j = 0; j < groups.length; j++){//For all columns:
+				if (inputData.get(0).get(i).equals(groups[j])){//If you find a group column entry, add it to the output data.
+					outputData[0][cc] = groups[j];
+					columnIndices[cc] = i;
+					cc++;
+					break;
+				}
+			}
+			if (inputData.get(0).get(i).equals(aggregation)){//If you find the aggregation column's entry, add it to the last column of the output data.
+				outputData[0][width-1] = aggregation;
+				columnIndices[width-1] = i;
+				aggregationFound = true;
+			}
+		}
+		for (int i = 1; i < inputData.size(); i++){
+			for (int j = 0; j < width; j++){
+				outputData[i][j] = inputData.get(i).get(columnIndices[j]);
+			}
+		}
+		if (cc < groups.length || !aggregationFound){
+			throw new IOException("File does not contain requested columns.");
+		}
+		return outputData;
 	}
-	
-	public static ArrayList<String[]> sum(String[][] table){
-		System.out.println("Into the sum method...");
-		return null;
+
+	public static String[][] process(String[][] inputData, String operation) throws IOException{//Before calling a function, runs in O(1) time.
+		ArrayList<String[]> groupEntries = new ArrayList<String[]>();
+		groupEntries.add(inputData[0]);
+		groupEntries.get(0)[inputData[0].length-1] = operation + "(" + inputData[0][inputData[0].length-1] + ")";
+		if (operation.equals("count")){
+			return count(inputData, groupEntries);
+		}
+		else if (operation.equals("sum")){
+			return sum(inputData, groupEntries);
+		}
+		else if (operation.equals("avg")){
+			return avg(inputData, groupEntries);
+		}
+		else if (operation.equals("count_distinct")){
+			return count_distinct(inputData, groupEntries);
+		}
+		else{
+			throw new IOException(operation + " is an invalid function.");
+		}
 	}
-	
-	public static ArrayList<String[]> avg(String[][] table){
-		System.out.println("Into the avg method...");
-		return null;
+
+	public static String[][] count(String[][] inputData, ArrayList<String[]> groupEntries){//Runs in O(n^2) time.
+		boolean present;
+		for (int i = 1; i < inputData.length; i++){
+			present = false;
+			for (int j = 1; j < groupEntries.size(); j++){
+				if (compareArrays(groupEntries.get(j), inputData[i], 0, inputData[i].length-1, 0, inputData[i].length-1)){
+					groupEntries.get(j)[inputData[i].length-1] = String.valueOf(Integer.parseInt(groupEntries.get(j)[inputData[i].length-1]) + 1);
+					present = true;
+					break;
+				}
+			}
+			if (!present){
+				groupEntries.add(inputData[i]);
+				groupEntries.get(groupEntries.size()-1)[inputData[i].length-1] = String.valueOf(1);
+			}
+		}
+		String[][] outputData = new String[groupEntries.size()][groupEntries.get(0).length];
+		for (int i = 0; i < outputData.length; i++){
+			outputData[i] = groupEntries.get(i);
+		}
+		return outputData;
 	}
-	
-	public static ArrayList<String[]> count_distinct(String[][] table){
-		System.out.println("Into the count_distinct method...");
-		return null;
+
+	public static String[][] sum(String[][] inputData, ArrayList<String[]> groupEntries) throws IOException{//Runs in O(n^2) time.
+		for (int i = 1; i < inputData.length; i++){
+			try{
+				Float.parseFloat(inputData[i][inputData[i].length-1]);
+			} catch (Exception e) {
+				throw new IOException("Cannot sum non-numerical data.");
+			}
+		}
+		boolean present;
+		String inputString;
+		float inputValue;
+		String outputString;
+		float outputValue;
+		for (int i = 1; i < inputData.length; i++){
+			present = false;
+			for (int j = 1; j < groupEntries.size(); j++){
+				if (compareArrays(groupEntries.get(j), inputData[i], 0, inputData[i].length-1, 0, inputData[i].length-1)){
+					inputString = inputData[i][inputData[i].length-1];
+					inputValue = Float.parseFloat(inputString);
+					outputString = groupEntries.get(j)[inputData[i].length-1];
+					outputValue = Float.parseFloat(outputString);
+					outputValue += inputValue;
+					outputString = String.valueOf(outputValue);
+					groupEntries.get(j)[inputData[i].length-1] = outputString;
+					present = true;
+					break;
+				}
+			}
+			if (!present){
+				groupEntries.add(inputData[i]);
+			}
+		}
+		String[][] outputData = new String[groupEntries.size()][groupEntries.get(0).length];
+		for (int i = 0; i < outputData.length; i++){
+			outputData[i] = groupEntries.get(i);
+		}
+		return outputData;
 	}
-	
-	//This method checks to see if 2 rows are the same for all columns
-	//except the agg column.
-	public static boolean compareRows(String[][] table, int row1, int row2) {
-		if(table[row1].length != table[row2].length) {
+
+	public static String[][] avg(String[][] inputData, ArrayList<String[]> groupEntries) throws IOException{//Runs in O(n^2) time.
+		for (int i = 1; i < inputData.length; i++){
+			try{
+				Float.parseFloat(inputData[i][inputData[i].length-1]);
+			} catch (Exception e) {
+				throw new IOException("Cannot average non-numerical data.");
+			}
+		}
+		boolean present;
+		String inputString;
+		float inputValue;
+		String outputString;
+		float outputValue;
+		ArrayList<Integer> count = new ArrayList<Integer>();
+		for (int i = 1; i < inputData.length; i++){
+			present = false;
+			for (int j = 1; j < groupEntries.size(); j++){
+				if (compareArrays(groupEntries.get(j), inputData[i], 0, inputData[i].length-1, 0, inputData[i].length-1)){
+					inputString = inputData[i][inputData[i].length-1];
+					inputValue = Float.parseFloat(inputString);
+					outputString = groupEntries.get(j)[inputData[i].length-1];
+					outputValue = Float.parseFloat(outputString);
+					outputValue += inputValue;
+					outputString = Float.toString(outputValue);
+					groupEntries.get(j)[inputData[i].length-1] = outputString;
+					count.set(j-1, count.get(j-1)+1);
+					present = true;
+					break;
+				}
+			}
+			if (!present){
+				groupEntries.add(inputData[i]);
+				count.add(1);
+			}
+		}
+		String[][] outputData = new String[groupEntries.size()][groupEntries.get(0).length];
+		outputData[0] = groupEntries.get(0);
+		for (int i = 1; i < outputData.length; i++){
+			outputData[i] = groupEntries.get(i);
+			outputData[i][groupEntries.get(0).length-1] = Float.toString(Float.parseFloat(outputData[i][groupEntries.get(0).length-1]) / count.get(i-1));
+		}
+		return outputData;
+	}
+
+	public static String[][] count_distinct(String[][] inputData, ArrayList<String[]> groupEntries){//Runs in O(n^2) time.
+		for (int i = 1; i < inputData.length; i++){//Copy inputData into groupEntries.
+			groupEntries.add(inputData[i]);
+		}
+		for (int i = 1; i < groupEntries.size(); i++){//Remove duplicates.
+			for (int j = i+1; j < groupEntries.size(); j++){
+				if (compareArrays(groupEntries.get(i), groupEntries.get(j), 0, groupEntries.get(i).length, 0, groupEntries.get(j).length)){
+					groupEntries.remove(j);
+					j--;
+				}
+			}
+		}
+		for (int i = 1; i < groupEntries.size(); i++){//Set all counts to 1.
+			groupEntries.get(i)[groupEntries.get(i).length-1] = String.valueOf(1);
+		}
+		for (int i = 1; i < groupEntries.size(); i++){//If rows of the same group are found, remove the second one and increment the first one's count.
+			for (int j = i+1; j < groupEntries.size(); j++){
+				if (compareArrays(groupEntries.get(i), groupEntries.get(j), 0, groupEntries.get(i).length-1, 0, groupEntries.get(j).length-1)){
+					groupEntries.get(i)[groupEntries.get(i).length-1] = String.valueOf(Integer.parseInt(groupEntries.get(i)[groupEntries.get(i).length-1])+1);
+					groupEntries.remove(j);
+					j--;
+				}
+			}
+		}
+		String[][] outputData = new String[groupEntries.size()][groupEntries.get(0).length];
+		for (int i = 0; i < outputData.length; i++){
+			outputData[i] = groupEntries.get(i);
+		}
+		return outputData;
+	}
+
+	public static boolean compareArrays(String[] array1, String[] array2, int l1, int r1, int l2, int r2){//Runs in O(1) time.
+		if (r1-l1!=r2-l2){
 			return false;
 		}
-		for(int i = 0; i < table[row1].length - 1; i++) {
-			if(table[row1][i] != table[row2][i]) {
+		for (int i = l1, j = l2; i < r1; i++, j++){
+			if (!array1[i].equals(array2[j])){
 				return false;
 			}
 		}
 		return true;
 	}
-	
+
+	public static void outputCSV(String[][] outputData){
+		String outputLine;
+		for (int i = 0; i < outputData.length; i++){
+			outputLine = new String();
+			for (int j = 0; j < outputData[i].length; j++){
+				outputLine += outputData[i][j] + ",";
+			}
+			if (outputLine.length() > 0){
+				outputLine = outputLine.substring(0, outputLine.length()-1);
+			}
+			System.out.println(outputLine);
+		}
+	}
+
+	public static void main(String[] args) throws IOException{//Total program runs in O(n^2) time.
+
+		//Process arguments.
+		String operation = args[0];
+
+		String aggregation = args[1];
+
+		String fileName = args[2];
+
+		String[] groups = new String[args.length-3];
+		for (int i = 3; i < args.length; i++){
+			groups[i-3] = args[i];
+		}
+
+		ArrayList<ArrayList<String>> table1 = readCSV(fileName);
+
+		String[][] table2 = reformat(table1, groups, aggregation);//Removes unnecessary columns.
+
+		String[][] table3 = process(table2, operation);//Processes data based on specified function.
+
+		outputCSV(table3);//Prints data in csv format to standard out.
+	}
 }
